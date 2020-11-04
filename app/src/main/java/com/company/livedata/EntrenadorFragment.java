4c3f848386eb049bc58e22ff59011ed264d33a83
nanonixx <nhid@elpuig.xeill.net>
2020-11-04 20:27:08 +0100

    Initial commit

---
+++ app/src/main/java/com/company/livedata/EntrenadorFragment.java
@@ -0,0 +1,25 @@
+package com.company.livedata;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.company.livedata.databinding.FragmentEntrenadorBinding;
+
+public class EntrenadorFragment extends Fragment {
+    private FragmentEntrenadorBinding binding;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return (binding = FragmentEntrenadorBinding.inflate(inflater, container, false)).getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
